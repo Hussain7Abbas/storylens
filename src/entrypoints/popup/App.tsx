9bcdecf26526cafa149d745c1f9bed078283fa89
hussain7abbas <hussain.jazing.21@gmail.com>
2025-09-20 12:18:15 +0300

    refactor(App): simplify layout by removing Center component and setting default type for App function

--- src/entrypoints/popup/App.tsx
+++ src/entrypoints/popup/App.tsx
@@ -2,26 +2,24 @@ import "@/utils/i18n";
 import "@mantine/core/styles.css";
 import "@/styles/global.css";
 import "./App.css";
-import { Center, MantineProvider, ScrollArea, Stack } from "@mantine/core";
+import { MantineProvider, ScrollArea, Stack } from "@mantine/core";
 
 import { Navbar } from "@/components/navbar";
 import { Router } from "./routers";
 
-function App({ type }: { type: "popup" | "options" }) {
+function App({ type = "popup" }: { type: "popup" | "options" }) {
 	return (
 		<MantineProvider>
-			<Center w="100vw" h="100vh">
-				<Stack
-					h={type === "popup" ? "30rem" : "100vh"}
-					w={type === "popup" ? "20rem" : "25rem"}
-					gap="xs"
-				>
-					<Navbar />
-					<ScrollArea>
-						<Router />
-					</ScrollArea>
-				</Stack>
-			</Center>
+			<Stack
+				h={type === "popup" ? "30rem" : "100vh"}
+				w={type === "popup" ? "20rem" : "100vw"}
+				gap="xs"
+			>
+				<Navbar />
+				<ScrollArea>
+					<Router />
+				</ScrollArea>
+			</Stack>
 		</MantineProvider>
 	);
 }
