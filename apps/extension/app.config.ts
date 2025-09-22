c06ca757c42dd482ebbba7815f404fddd6859db7
hussain7abbas <hussain.jazing.21@gmail.com>
2025-09-22 12:17:21 +0300

    refactor(extension): update app configuration to use environment variables for WXT_API_URL and remove deprecated properties

--- apps/extension/app.config.ts
+++ apps/extension/app.config.ts
@@ -1,13 +1,11 @@
 import { defineAppConfig } from '#imports';
-
+import { env } from './src/env';
 declare module 'wxt/utils/define-app-config' {
   export interface WxtAppConfig {
-    apiKey?: string;
-    skipWelcome: boolean;
+    WXT_API_URL: typeof env.WXT_API_URL;
   }
 }
 
 export default defineAppConfig({
-  apiKey: import.meta.env.WXT_API_KEY,
-  skipWelcome: import.meta.env.WXT_SKIP_WELCOME === 'true',
+  WXT_API_URL: env.WXT_API_URL,
 });
---
+++ apps/extension/src/env.ts
@@ -0,0 +1,12 @@
+import { createEnv } from '@t3-oss/env-core';
+import { z } from 'zod';
+
+export const env = createEnv({
+  runtimeEnv: process.env,
+  emptyStringAsUndefined: true,
+  clientPrefix: 'WXT_',
+
+  client: {
+    WXT_API_URL: z.string(),
+  },
+});
